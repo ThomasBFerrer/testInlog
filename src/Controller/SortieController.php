5e92a33ab6f8100739d671bdf461dc9892c263c2
Thomas Ferrer <91891231+ThomasBFerrer@users.noreply.github.com>
2022-04-28 11:27:28 +0200

    commit

---
+++ src/Controller/SortieController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Sortie;
+use App\Entity\User;
+use App\Form\SortieType;
+use App\Repository\SortieRepository;
+use App\Repository\UserRepository;
+use App\Repository\UtilisateurRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
+
+#[Route('/sortie')]
+class SortieController extends AbstractController
+{
+    private Security $security;
+
+    public function __construct(Security $security){
+        $this->security = $security;
+    }
+
+    #[Route('/', name: 'app_sortie_index', methods: ['GET'])]
+    public function index(SortieRepository $sortieRepository, UtilisateurRepository $utilisateurRepository): Response
+    {
+        return $this->render('sortie/index.html.twig', [
+            'sorties' => $sortieRepository->findBy(['utilisateur'=>$this->security->getUser()->getId()]),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_sortie_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, SortieRepository $sortieRepository, UtilisateurRepository $utilisateurRepository): Response
+    {
+        $sortie = new Sortie();
+        $form = $this->createForm(SortieType::class, $sortie);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $organizer = $utilisateurRepository->find($this->security->getUser()->getId());
+            $sortie->setUtilisateur($organizer);
+            $sortieRepository->add($sortie);
+            return $this->redirectToRoute('app_sortie_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('sortie/new.html.twig', [
+            'sortie' => $sortie,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_sortie_show', methods: ['GET'])]
+    public function show(Sortie $sortie): Response
+    {
+        return $this->render('sortie/show.html.twig', [
+            'sortie' => $sortie,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_sortie_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Sortie $sortie, SortieRepository $sortieRepository): Response
+    {
+        $form = $this->createForm(SortieType::class, $sortie);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $sortieRepository->add($sortie);
+            return $this->redirectToRoute('app_sortie_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('sortie/edit.html.twig', [
+            'sortie' => $sortie,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_sortie_delete', methods: ['POST'])]
+    public function delete(Request $request, Sortie $sortie, SortieRepository $sortieRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$sortie->getId(), $request->request->get('_token'))) {
+            $sortieRepository->remove($sortie);
+        }
+
+        return $this->redirectToRoute('app_sortie_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Entity/Sortie.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SortieRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SortieRepository::class)]
+class Sortie
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $type;
+
+    #[ORM\Column(type: 'datetime')]
+    private $date;
+
+    #[ORM\Column(type: 'time', nullable: true)]
+    private $duree;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $distance;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $com;
+
+    #[ORM\ManyToOne(targetEntity: Utilisateur::class, inversedBy: 'Sortie')]
+    private $utilisateur;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(?string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getDuree(): ?\DateTimeInterface
+    {
+        return $this->duree;
+    }
+
+    public function setDuree(\DateTimeInterface $duree): self
+    {
+        $this->duree = $duree;
+
+        return $this;
+    }
+
+    public function getDistance(): ?string
+    {
+        return $this->distance;
+    }
+
+    public function setDistance(string $distance): self
+    {
+        $this->distance = $distance;
+
+        return $this;
+    }
+
+    public function getCom(): ?string
+    {
+        return $this->com;
+    }
+
+    public function setCom(?string $com): self
+    {
+        $this->com = $com;
+
+        return $this;
+    }
+
+    public function getUtilisateur(): ?Utilisateur
+    {
+        return $this->utilisateur;
+    }
+
+    public function setUtilisateur(?Utilisateur $utilisateur): self
+    {
+        $this->utilisateur = $utilisateur;
+
+        return $this;
+    }
+}
